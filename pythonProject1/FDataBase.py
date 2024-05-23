4d3914c941dd13978d072b84f8ee88c94c819466
Keroml <147330566+Keroml@users.noreply.github.com>
2024-05-23 23:32:56 +0500

    Add files via upload

---
+++ pythonProject1/FDataBase.py
@@ -0,0 +1,109 @@
+import sqlite3
+import time
+import math
+import re
+from flask import url_for
+
+class FDataBase:
+    def __init__(self, db):
+        self.__db = db
+        self.__cur = db.cursor()
+
+    def getMenu(self):
+        sql = '''SELECT * FROM mainmenu'''
+        try:
+            self.__cur.execute(sql)
+            res = self.__cur.fetchall()
+            if res: return res
+        except:
+            print("Ошибка чтения из БД")
+        return []
+
+
+    def addPost(self, users, classes, themes, text):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO posts VALUES(NULL,?, ?, ?, ?, ?)", (users, classes, themes, text, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления заданий в БД " + str(e))
+            return False
+
+        return True
+
+    def getPost(self, postId):
+        try:
+            self.__cur.execute(f"SELECT classes, themes, text FROM posts WHERE id = {postId} LIMIT 1")
+            res = self.__cur.fetchone()
+            if res:
+                return res
+        except sqlite3.Error as e:
+            print("Ошибка получения заданий из БД " + str(e))
+
+        return (False, False)
+
+
+    def getPostsAnonce(self, postUser):
+        try:
+            self.__cur.execute(f"SELECT id, classes, themes, text FROM posts WHERE users = {postUser} ORDER BY time DESC")
+            res = self.__cur.fetchall()
+            if res: return res
+        except sqlite3.Error as e:
+            print("Ошибка получения заданий из БД " + str(e))
+
+        return []
+
+    def getPostById(self, id_post):
+        cur = self._db.cursor()
+        cur.execute("SELECT * FROM posts WHERE id=?", (id_post,))
+        row = cur.fetchone()
+        if row is None:
+            return None
+        return dict(row)
+
+
+    def addUser(self, name, email, hpsw):
+        try:
+            self.__cur.execute(f"SELECT COUNT() as `count` FROM users WHERE email LIKE '{email}'")
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print("Пользователь с таким email уже существует")
+                return False
+
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO users VALUES(NULL, ?, ?, ?, ?)", (name, email, hpsw, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления пользователя в БД " + str(e))
+            return False
+
+        return True
+
+    def getUser(self, user_id):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE id = '{user_id}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД " + str(e))
+
+        return False
+
+    def getUserByEmail(self, email):
+        try:
+            self.__cur.execute(f"SELECT * FROM users WHERE email = '{email}' LIMIT 1")
+            res = self.__cur.fetchone()
+            if not res:
+                print("Пользователь не найден")
+                return False
+
+            return res
+        except sqlite3.Error as e:
+            print("Ошибка получения данных из БД " + str(e))
+
+        return False
+
---
+++ pythonProject1/main.py
@@ -0,0 +1,220 @@
+from io import BytesIO
+import sqlite3
+import os
+import codecs
+from flask import Flask, render_template, url_for, request, session, redirect, abort, g, flash, make_response, send_file, Response, send_from_directory
+from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from UserLogin import UserLogin
+import re
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+
+
+DATABASE = 'flsite.db'
+DEBUG = True
+SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
+
+#print( url_for('оброботчик(имя функции в код вызывается') ) возвращает url адрес
+app=Flask(__name__)
+app.config.from_object(__name__)
+
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path,'flsite.db')))#путь к бд(в раб каталоге)
+
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Авторизуйтесь для доступа"
+login_manager.login_message_category = "success"
+
+
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    print("load_user")
+    return UserLogin().fromDB(user_id, dbase)
+
+def connect_db():#создание бд\установление соединения
+    conn = sqlite3.connect(app.config['DATABASE'])#путь рассположения бд
+    conn.row_factory = sqlite3.Row#записи бд будут ввиде словаря, а не кортежей
+    return conn#возвращает установленное соединение
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+def get_db():
+    '''Соединение с БД, если оно еще не установлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+dbase = None
+@app.before_request
+def before_request():
+    """Установление соединения с БД перед выполнением запроса"""
+    global dbase
+    db = get_db()
+    dbase = FDataBase(db)
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+@app.route("/")
+@login_required
+def index():
+    return render_template('index.html', menu = dbase.getMenu(), post=dbase.getPostsAnonce(current_user.get_id()), title= 'psuTests')
+
+@app.route("/help")
+def help():
+    if current_user.is_authenticated:
+        return render_template('help2.html', menu=dbase.getMenu(), title='Инструкция')
+
+    return render_template('help.html', menu = dbase.getMenu(), title= 'Инструкция')
+
+def process_text(text):
+
+    def process_line(line):
+        line = line.strip()
+
+
+        if "==" in line:
+            return line
+        elif re.match(r"^\d+[)]\s*\w+", line):
+
+            return line
+        elif re.match(r"[а-яА-Я][)]\s*\w+", line):
+
+
+            match = re.match(r"^[а-яА-Я][)]\s*[а-яА-Я0-9,\s,\-,\–]*\*+$", line)
+
+            if match:
+                return line.strip()[:-1]
+            else:
+                return line[0] + ")"
+        elif re.match(r"^\[.*\]$", line):
+            return line.strip("[]")
+        return None
+
+
+    lines = text.split("\n")
+    processed_lines = [process_line(line) for line in lines]
+    processed_lines = [line for line in processed_lines if line != None]
+
+
+    result =  "\n".join(processed_lines)
+
+    return result
+
+
+@app.route("/upload", methods=["POST", "GET"])
+@login_required
+def upload():
+    if request.method == 'POST':
+        if len(request.form['post']) > 10:
+            post=process_text(request.form['post'])
+            res = dbase.addPost(current_user.get_id(),request.form['classes'], request.form['themes'], post)
+            if not res:
+                flash("Ошибка добавления заданий", "error")
+                return redirect(url_for('upload'))
+            flash("Задания добавлены успешно", "success")
+        else:
+            flash("Ошибка добавления заданий", "error")
+
+    return render_template('load.html', menu=dbase.getMenu(), title="Загрузка заданий")
+
+@app.route("/post/<int:id_post>")
+@login_required
+def showPost(id_post):
+    classes, themes, post = dbase.getPost(id_post)
+    if not classes or not themes:
+        abort(404)
+    return render_template('post.html', menu=dbase.getMenu(), classes=classes, title= themes, post=post,id_post=id_post)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+
+    if request.method == "POST":
+        user = dbase.getUserByEmail(request.form['email'])
+        if user and check_password_hash(user['psw'], request.form['psw']):
+            userlogin = UserLogin().create(user)
+            rm = True if request.form.get('remainme') else False
+            login_user(userlogin, remember=rm)
+            return redirect(request.args.get("next") or url_for("index"))
+
+        flash("Неверный логин/пароль", "error")
+
+    return render_template("login.html", menu=dbase.getMenu(), title="Авторизация")
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        session.pop('_flashes', None)
+        if len(request.form['name']) > 4 and len(request.form['email']) > 4 \
+                and len(request.form['psw']) > 4 and request.form['psw'] == request.form['psw2']:
+            hash = generate_password_hash(request.form['psw'])
+            res = dbase.addUser(request.form['name'], request.form['email'], hash)
+            if res:
+                flash("Вы успешно зарегистрированы", "success")
+                return redirect(url_for('login'))
+            else:
+                flash("Ошибка при добавлении в БД", "error")
+        else:
+            flash("Неверно заполнены поля", "error")
+
+    return render_template("register.html", menu=dbase.getMenu(), title="Регистрация")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("Вы вышли из аккаунта", "success")
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template("profile.html", menu=dbase.getMenu(), title="Профиль")
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=dbase.getMenu()), 404
+
+
+@app.route("/post1/<int:id_post>")
+@login_required
+def post(id_post):
+    result = dbase.getPost(id_post)
+    if result:
+        x1 = result[0]
+        x2 = result[1]
+        with open(f"static/{x1}-{x2}.doc", 'w') as file:
+            file.writelines([result[2]])
+        directory = 'static'
+        # Имя файла, который нужно скачать
+        filename = f"{x1}-{x2}.doc"
+        # Отправляем файл пользователю
+        return send_from_directory(directory, filename, as_attachment=True)
+
+    else:
+        flash("Пост с указанным ID не найден.", "success")
+        return render_template('page404.html', title="Страница не найдена", menu=dbase.getMenu()), 404
+
+
+
+
+if __name__ == "__main__" :
+    app.run(debug=True) #после завершения написания сайта поменять на false!!!
